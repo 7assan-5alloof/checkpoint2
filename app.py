bb0331b59dccf154ed929ea0a5f67b6a714bb8c9
7assan-5alloof <hassankhalloof20@gmail.com>
2021-02-01 09:04:54 +0400

    Finish Checkpoint

---
+++ app.py
@@ -0,0 +1,21 @@
+herName = "Fatima Abdulla Sayed Mansouri"
+myName = "Hassan Abdulrahman Mustafa Khalloof"
+herAge = 15
+myAge = 13
+herBirthMonth = "December"
+myBirthMonth = "January"
+herGender = 'F'
+myGender = 'M'
+herHeight = 1.54
+myHeight = 1.76
+print(herName)
+print(f"Age: {herAge}")
+print(f"Birth Month: {herBirthMonth}")
+print(f"Gender: {herGender}")
+print(f"Height: {herHeight}")
+print("---------------------------------------")
+print(myName)
+print(f"Age: {myAge}")
+print(f"Birth Month: {myBirthMonth}")
+print(f"Gender: {myGender}")
+print(f"Height: {myHeight}")
